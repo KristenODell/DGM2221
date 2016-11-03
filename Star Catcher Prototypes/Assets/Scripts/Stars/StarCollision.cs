e4927c8596276873c27e1ba37ef1c06002170a79
Unknown <little_miss_kris07@hotmail.com>
2016-11-02 22:29:42 -0600

    Stars not working

--- Star Catcher Prototypes/Assets/Scripts/Stars/StarCollision.cs
+++ Star Catcher Prototypes/Assets/Scripts/Stars/StarCollision.cs
@@ -5,17 +5,19 @@ public class StarCollision : MonoBehaviour
 {
 
     private StarCounter starCounter;
+    public Transform myTransform;
 
 	// Use this for initialization
 	void Awake ()
     {
         starCounter = GameObject.Find("Star Counter").GetComponent<StarCounter>();
+        myTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        myTransform.position = myTransform.position + Vector3.zero;
 	}
     
     //void OnCollisionEnter(Collision collision)
