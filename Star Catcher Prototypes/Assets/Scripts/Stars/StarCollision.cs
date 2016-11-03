d3b53c720ccc1ef09f198fce186276e25986e194
Unknown <little_miss_kris07@hotmail.com>
2016-11-02 22:41:49 -0600

    Star Collision Script Changes

--- Star Catcher Prototypes/Assets/Scripts/Stars/StarCollision.cs
+++ Star Catcher Prototypes/Assets/Scripts/Stars/StarCollision.cs
@@ -6,27 +6,37 @@ public class StarCollision : MonoBehaviour
 
     private StarCounter starCounter;
 
-	// Use this for initialization
-	void Awake ()
+
+    // Use this for initialization
+    void Awake()
     {
         starCounter = GameObject.Find("Star Counter").GetComponent<StarCounter>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+    }
 
-	}
-    
-    //void OnCollisionEnter(Collision collision)
-    void OnTriggerEnter()
+    // Update is called once per frame
+    void Update()
     {
-        //if(collision.gameObject.tag == "Player")
-        // {
-        starCounter.starCount++;
+        transform.Translate(0, 0, 0);
+    }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if(collision.gameObject.tag == "Player")
+    //    {
+    //        starCounter.starCount++;
+    //        gameObject.SetActive(false);
+    //        //Destroy(collision.gameObject);
+    //    }
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            starCounter.starCount++;
             gameObject.SetActive(false);
-        //Destroy(collision.gameObject);
-        //}
+        }
     }
 
 }
+
