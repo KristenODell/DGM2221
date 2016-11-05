58f9ce5289db8517aa667d3bce822776bfcde2b2
Unknown <little_miss_kris07@hotmail.com>
2016-11-05 11:58:49 -0600

    Resume button after pause

--- Star Catcher Prototypes/Assets/Scripts/PauseGame.cs
+++ Star Catcher Prototypes/Assets/Scripts/PauseGame.cs
@@ -15,6 +15,24 @@ public class PauseGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Pause();
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    if (canvas.gameObject.activeInHierarchy == false)
+        //    {
+        //        canvas.gameObject.SetActive(true);
+        //        Time.timeScale = 0;
+        //    }
+        //    else
+        //    {
+        //        canvas.gameObject.SetActive(false);
+        //        Time.timeScale = 1;
+        //    }
+        //}
+    }
+
+    public void Pause()
+    {
         if (Input.GetKeyDown(KeyCode.P))
         {
             if (canvas.gameObject.activeInHierarchy == false)
