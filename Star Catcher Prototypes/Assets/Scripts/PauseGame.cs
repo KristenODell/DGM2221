bb5381035a465d2d347b510cc0c755e3d9c25b6b
Unknown <little_miss_kris07@hotmail.com>
2016-11-05 11:36:13 -0600

    Fixing script

--- Star Catcher Prototypes/Assets/Scripts/PauseGame.cs
+++ Star Catcher Prototypes/Assets/Scripts/PauseGame.cs
@@ -6,28 +6,29 @@ public class PauseGame : MonoBehaviour
     public Transform canvas;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
     {
-	    //if(Input.GetKeyDown(KeyCode.P))
-     //   {
-     //       if(canvas.gameObject.activeInHierarchy == false)
-     //       {
-     //           canvas.gameObject.SetActive(true);
-     //           Time.timeScale = 0;
-     //       }
-     //       else
-     //       {
-     //           canvas.gameObject.SetActive(false);
-     //           Time.timeScale = 1;
-     //       }
-     //   }
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.P))
+        //   {
+        //       if(canvas.gameObject.activeInHierarchy == false)
+        //       {
+        //           canvas.gameObject.SetActive(true);
+        //           Time.timeScale = 0;
+        //       }
+        //       else
+        //       {
+        //           canvas.gameObject.SetActive(false);
+        //           Time.timeScale = 1;
+        //       }
+        //   }
+    }
 
     void PauseTheGame()
     {
@@ -44,4 +45,5 @@ public class PauseGame : MonoBehaviour
                 Time.timeScale = 1;
             }
         }
+    }
 }
