0436b1697a3418d9fab50eec0da33a998463e039
Unknown <little_miss_kris07@hotmail.com>
2016-11-08 15:37:37 -0700

    AI Wolf

---
+++ Star Catcher Prototypes/Assets/AI/WolfController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfController : MonoBehaviour
+{
+    public CharacterController myCC;
+    public float gravity = 1;
+    public Vector3 tempPos;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        tempPos.y -= gravity * Time.deltaTime;
+        myCC.Move(tempPos);
+    }
+}
