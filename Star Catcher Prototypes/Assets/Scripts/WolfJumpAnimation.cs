6cc86bb1df25fe42617c7a744a383828b285a8ee
Unknown <little_miss_kris07@hotmail.com>
2016-11-11 16:19:33 -0700

    Fixing wolf animation issues

---
+++ Star Catcher Prototypes/Assets/Scripts/WolfJumpAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfJumpAnimation : MonoBehaviour
+{
+    public Animator myAnim;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+    void OnTriggerEnter ()
+    {
+        myAnim.SetBool("jump", true);
+    }
+
+    void OnTriggerExit()
+    {
+        myAnim.SetBool("jump", false);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
