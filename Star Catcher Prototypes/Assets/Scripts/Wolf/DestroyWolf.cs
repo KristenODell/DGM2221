1453d57b5ef1359fef625559be927fd114299d74
Unknown <little_miss_kris07@hotmail.com>
2016-11-11 18:01:32 -0700

    Wolf Spawning

---
+++ Star Catcher Prototypes/Assets/Scripts/Wolf/DestroyWolf.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWolf : MonoBehaviour
+{
+    public Transform wolf;
+    public Transform startPoint;
+   
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    void OnTriggerEnter ()
+    {
+        wolf.position = startPoint.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+}
---
+++ Star Catcher Prototypes/Assets/Scripts/Wolf/EnemyStartTime.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStartTime : MonoBehaviour
+{
+    public float startTime = 15;
+    public GameObject enemy;
+
+    IEnumerator StartEnemy()
+    {
+        int i = 1;
+        while (i > 0)
+        {
+            yield return new WaitForSeconds(startTime);
+            enemy.SetActive(true);
+            i--;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(StartEnemy());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
