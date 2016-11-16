defad1b41ab7062eb6c8949fbb9a91f4eb893aa9
Unknown <little_miss_kris07@hotmail.com>
2016-11-15 18:47:37 -0700

    End Game Screen

---
+++ Star Catcher Prototypes/Assets/Scripts/End Game Screen Scripts/GetFinalScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetFinalScore : MonoBehaviour
+{
+    public Text finalScore;
+    private StarCounter starCount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        finalScore = GetComponent<Text>();
+        starCount = GameObject.Find("Star Counter").GetComponent<StarCounter>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        finalScore.text = "FINAL SCORE : " + starCount.starCount;
+        Time.timeScale = 0;
+    }
+}
